092ca2cc82f67c4c73778fca66fd7a112e43c70b
Safonov Iya <yagitarist17@gmail.com>
2022-03-21 11:22:48 +0300

    add scroll and timer

---
+++ script/scroll.js
@@ -0,0 +1,44 @@
+// const links = document.querySelectorAll('.menu-list__link')
+// const testDrive = document.querySelector('main__button')
+
+// const allLinks = [...links, testDrive];
+
+// allLinks.forEach(link => {
+//   link.addEventListener('click', (event) => {
+//     event.preventDefault()
+    
+//     const id = link.getAttribute('href').substring(1)
+//     const section = document.getElementById(id)
+
+//       if (section) {
+//         seamless.scrollIntoView(section, {
+//           behavior: "smooth",
+//           block: "center",
+//           inline: "center",
+//       })
+//     }
+//   })
+// })
+
+const links = document.querySelectorAll('.menu-list__link');
+const btn = document.querySelector('.main__button');
+const scrl = document.querySelector('.main__scroll');
+
+const allLinks = [...links, btn, scrl];
+
+allLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+
+    const id = link.getAttribute('href').substring(1);
+    const section = document.getElementById(id);
+
+    if (section) {
+        seamless.scrollIntoView(section, {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    }
+  });
+});
